5dc62ba2b167acb38c1a1590f0b72be56aac3cd5
Dyeizon <34202471+Dyeizon@users.noreply.github.com>
2024-03-18 09:46:50 -0300

    Add files via upload

--- ex002/server/index.js
+++ ex002/server/index.js
@@ -1,9 +1,33 @@
-const soap = require('soap')
+const soap = require('soap');
 
 const service = {
     MDCCalculator: {
         MDCPort: {
-            
+            CalculateMDC: function(args, callback) {
+                const x = args.x;
+                const y = args.y;
+
+                var gcd = function(a, b) {
+                    if (!b) {
+                        return a;
+                    }
+                    
+                    return gcd(b, a % b);
+                }
+
+                const mdc = gcd(x, y);
+
+                const result = {
+                    MDC: mdc,
+                }
+
+                callback(null, result);
+            }
         }
     }
 }
+
+const xml = require('fs').readFileSync('mdc.wsdl', 'utf8');
+const server = soap.listen({ port: 3000, path: '/mdc', xml: xml }, function() {
+  console.log('SOAP server running at http://localhost:3000/mdc?wsdl');
+});
